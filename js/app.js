e55532c75c65e014ae2f354cfb0ae3e7fabe901c
2smallears <2smallears@users.noreply.github.com>
2016-08-03 22:18:02 +0800

    button toggle edit and preview

--- js/app.js
+++ js/app.js
@@ -1,9 +1,43 @@
 const App = React.createClass({
+   getInitialState: function () {
+       return {
+           isEdit: true
+       }
+   },
+   toggle: function () {
+       this.setState({
+           isEdit: !this.state.isEdit
+       });
+   },
    render: function () {
+       const isEdit = this.state.isEdit;
        return <div>
-           start!
+           <button onClick={this.toggle}>{isEdit ? 'Preview' : 'Edit'}</button>
+           <div className={isEdit ? "" : "hidden"}>
+               <Edit />
+           </div>
+           <div className={isEdit ? "hidden" : ""}>
+               <Preview />
+           </div>
        </div>;
    }
 });
+
+const Edit = React.createClass({
+    render: function () {
+        return <div>
+            edit
+        </div>;
+    }
+});
+
+const Preview = React.createClass({
+    render: function () {
+        return <div>
+            preview
+        </div>;
+    }
+});
+
 
 ReactDOM.render(<App />, document.getElementById('content'));
